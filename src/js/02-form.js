c9d189c11fda248da2fa4f69cdfcb51ef187fd2d
OlegZhdanenko <125878669+OlegZhdanenko@users.noreply.github.com>
2023-12-22 13:39:50 +0300

    sum

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -3,31 +3,36 @@ const form = document.querySelector(".feedback-form");
 form.addEventListener("submit", onSubmit);
 form.addEventListener("input", onForm);
 
-
-
-
 let formData = {};
 function onForm(event) {
     formData[event.target.name] = event.target.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-    
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));  
 };
 
 function onSubmit(event) {
-    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-    event.preventDefault();
-    event.currentTarget.reset();
-    
+    const formOnSubmit = event.target;
+    const emailOnSubmit = formOnSubmit.elements.email.value;
+    const messegeOnSubmit = formOnSubmit.elements.message.value;
+    if (emailOnSubmit === "" || messegeOnSubmit === "") {
+    return alert("Заповни всі поля")
+    } else {
+        console.log(JSON.parse(localStorage.getItem('feedback-form-state').trim()));
+        event.preventDefault();
+        event.currentTarget.reset();
+    };
 };
+
 function dataFromLocalStorage() {
     const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-    let email = document.querySelector('.feedback-form input');
-    let message = document.querySelector('.feedback-form textarea');
-    if (data !== "" && data !=="") {
-    email.value = data.email;
-    message.value = data.message;
-    }  else {
-    alert("Заповни всі поля!")
+    const email = document.querySelector('.feedback-form input');
+    const message = document.querySelector('.feedback-form textarea');
+    console.log(data.email);
+    console.log(data.message);
+    if (data.email==="" || data.message==="") {
+       return
+    } else {
+         email.value = data.email;
+        message.value = data.message;
     };
 };
 dataFromLocalStorage();
